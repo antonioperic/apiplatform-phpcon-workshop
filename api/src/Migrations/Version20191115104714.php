aea950eed48410ee8cf289eeb424f47c42598f09
marek <marek.firlejczyk@rst-it.com>
2019-11-15 11:50:02 +0100

    add validation

--- api/src/Migrations/Version20191115104714.php
+++ api/src/Migrations/Version20191115104714.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191115103011 extends AbstractMigration
+final class Version20191115104714 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -23,12 +23,10 @@ final class Version20191115103011 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('CREATE SEQUENCE book_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE greeting_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE author_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE review_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE book (id INT NOT NULL, author_id INT DEFAULT NULL, isbn VARCHAR(13) NOT NULL, title VARCHAR(255) NOT NULL, abstract TEXT NOT NULL, publication_date DATE NOT NULL, average_review_rate DOUBLE PRECISION NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE book (id INT NOT NULL, author_id INT DEFAULT NULL, isbn VARCHAR(13) NOT NULL, title VARCHAR(255) NOT NULL, abstract TEXT NOT NULL, publication_date DATE NOT NULL, average_review_rate DOUBLE PRECISION NOT NULL, description TEXT NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_CBE5A331F675F31B ON book (author_id)');
-        $this->addSql('CREATE TABLE greeting (id INT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE TABLE author (id INT NOT NULL, firstname VARCHAR(255) NOT NULL, lastname TEXT NOT NULL, birthday DATE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE TABLE review (id INT NOT NULL, book_id INT DEFAULT NULL, author VARCHAR(255) NOT NULL, review TEXT NOT NULL, rate DOUBLE PRECISION NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_794381C616A2B381 ON review (book_id)');
@@ -45,11 +43,9 @@ final class Version20191115103011 extends AbstractMigration
         $this->addSql('ALTER TABLE review DROP CONSTRAINT FK_794381C616A2B381');
         $this->addSql('ALTER TABLE book DROP CONSTRAINT FK_CBE5A331F675F31B');
         $this->addSql('DROP SEQUENCE book_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE greeting_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE author_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE review_id_seq CASCADE');
         $this->addSql('DROP TABLE book');
-        $this->addSql('DROP TABLE greeting');
         $this->addSql('DROP TABLE author');
         $this->addSql('DROP TABLE review');
     }
